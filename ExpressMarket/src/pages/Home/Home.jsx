e9158c4dec3e9dc190071b179a5fcb7721a9c10d
00048020@uca.edu.sv <71047132+bonilla66@users.noreply.github.com>
2024-06-17 22:59:19 -0600

    Changing home page

--- ExpressMarket/src/pages/Home/Home.jsx
+++ ExpressMarket/src/pages/Home/Home.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from "../../components/Footer/Footer"
 import Navbar from "../../components/Navbar/Navbar"
 import Carousel from '../../components/Carousel/Carousel'
 import Slider from '../../components/Carousel/Slider'
-import { useEffect, useState } from 'react';
 import { getEvents } from '../../services/UserService'
 import NavbarAdmin from '../../components/NavbarAdmin/NavbarAdmin';
+import logo from "../../assets/img/ExpressMarketLogo.png";
+import lacteos from "../../assets/img/lacteos2.jpg";
+import fyv from "../../assets/img/frutasyverduras.jpg";
+import personal from "../../assets/img/cuidadopersonal.jpg";
+import hogar from "../../assets/img/cuidadodelhogar.png";
+import HomeProduct from '../../components/HomeProduct/HomeProduct';
+import alpina from "../../assets/img/alpina.jpg";
+import colgate from "../../assets/img/colgate.jpg";
+import cubata from "../../assets/img/cubata.jpg";
+import diamond from "../../assets/img/diamond.jpg";
+import banner from "../../assets/img/bannerhome.png";
+
 
 function Home() {
 
     const [images, setImages] = useState([]);
 
-    useEffect(() => {   
-        if (images.length == 0) {
+    useEffect(() => {
+        if (images.length === 0) {
             async function fetchAllEvents() {
                 let response = await getEvents();
                 if (response.success) {
@@ -21,46 +32,77 @@ function Home() {
             }
             fetchAllEvents();
         }
-    }), [setImages];
-
+    }, [images]);
+    
     return (
         <div>
             <div className="flex flex-wrap justify-center">
-                <Navbar/>
+                <Navbar />
                 <Carousel />
-                <div className="w-full md:w-1/3"></div>
-                <div className="w-full">
-                    <div className="w-2/3 mx-auto text-3xl text-center font-montserrat m-14 text-pure-indigo">
-                        <p className='mb-2'>
-                            En Ticketify podrás encontrar las entradas para los mejores conciertos, festivales, deportes y eventos en vivo de todo
-                            el país.
-                        </p>
-                        <p className='mb-4'>
-                            No pierdas la oportunidad de vivir una experiencia única.
-                        </p>
-                        <p className='mb-4'>
-                            Compra tus entradas ahora y prepárate para disfrutar de los eventos más emocionantes del año.
-                        </p>
-                        <p className='mb-4 font-medium'>
-                            ¡Únete a la diversión en vivo con nosotros!
-                        </p>
-                    </div>
+            </div>
+            <div className="px-4 mx-auto mt-8 text-center max-w-7xl">
+                <h2 className="mb-4 text-3xl font-normal font-montserrat">Categorías destacadas</h2>
+                <div className="flex justify-center gap-4">
+                    <button className="relative w-1/4 overflow-hidden bg-center bg-cover rounded-lg shadow-lg" style={{ backgroundImage: `url(${lacteos})`, height: '220px' }}>
+                        <div className="absolute inset-0 flex flex-col items-center justify-center text-white transition duration-300 bg-green-700 bg-opacity-0 hover:bg-opacity-50">
+                            <h3 className="text-xl font-semibold font-montserrat">Lácteos</h3>
+                        </div>
+                    </button>
+                    <button className="relative w-1/4 overflow-hidden bg-center bg-cover rounded-lg shadow-lg" style={{ backgroundImage: `url(${fyv})`, height: '220px' }}>
+                        <div className="absolute inset-0 flex flex-col items-center justify-center text-white transition duration-300 bg-green-700 bg-opacity-0 hover:bg-opacity-50">
+                            <h3 className="text-xl font-semibold font-montserrat">Frutas y Verduras</h3>
+                        </div>
+                    </button>
+                    <button className="relative w-1/4 overflow-hidden bg-center bg-cover rounded-lg shadow-lg" style={{ backgroundImage: `url(${personal})`, height: '220px' }}>
+                        <div className="absolute inset-0 flex flex-col items-center justify-center text-white transition duration-300 bg-green-700 bg-opacity-0 hover:bg-opacity-50">
+                            <h3 className="text-xl font-semibold font-montserrat">Cuidado Personal</h3>
+                        </div>
+                    </button>
+                    <button className="relative w-1/4 overflow-hidden bg-center bg-cover rounded-lg shadow-lg" style={{ backgroundImage: `url(${hogar})`, height: '220px' }}>
+                        <div className="absolute inset-0 flex flex-col items-center justify-center text-white transition duration-300 bg-green-700 bg-opacity-0 hover:bg-opacity-50">
+                            <h3 className="text-xl font-semibold font-montserrat">Cuidado del Hogar</h3>
+                        </div>
+                    </button>
                 </div>
-                <div className="w-full md:w-1/3"></div>
+                <img  className=" my-9" src={banner} />
             </div>
-
-            <hr className="m-2 bg-silver" />
-
-            <div>
-                <a href={'/events'}>
-                    <p className='mt-6 ml-4 text-5xl font-extrabold text-pure-indigo font-montserrat'>Próximos eventos </p>
-                </a>
+            <div className='text-center'>
+                <p className= "mt-5 mb-2 text-3xl font-normal font-montserrat">Productos de temporada</p>
+                <div className="grid grid-cols-1 gap-6 mx-auto mt-6 max-w-7xl sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
+                    <HomeProduct
+                            image={alpina}
+                            name="Leche Entera"
+                            category="Lácteos"
+                            price={2.99}
+                            savingsPercentage={10}
+                    />
+                    <HomeProduct
+                            image={cubata}
+                            name="Manzanas"
+                            category="Frutas y Verduras"
+                            price={1.49}
+                            savingsPercentage={10}
+                    />
+                    <HomeProduct
+                            image={diamond}
+                            name="Jabón Corporal"
+                            category="Cuidado Personal"
+                            price={3.99}
+                            savingsPercentage={10}
+                    />
+                    <HomeProduct
+                            image={colgate}
+                            name="Detergente"
+                            category="Cuidado del Hogar"
+                            price={4.49}
+                            savingsPercentage={10}
+                    />
+                </div>
             </div>
-
-            <Slider images={images}/>
+            <Slider images={images} />
             <Footer />
         </div>
     )
 }
 
-export default Home
+export default Home;
